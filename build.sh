c32ef314a0a63fa492092eafeaa10291bfa4469d
Alexander Samusevich <archer.developer@gmail.com>
2020-11-22 23:53:31 +0300

    Docker files were renamed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-docker build -f Dockerfile.fpm --build-arg ENVIRONMENT=development -t mgdteam/php:7.4.4-fpm-nginx .
-docker build -f Dockerfile.cli --build-arg ENVIRONMENT=development -t mgdteam/php:7.4.4-cli .
+docker build -f Dockerfile.7.4.4-fpm --build-arg ENVIRONMENT=development -t mgdteam/php:7.4.4-fpm-nginx .
+docker build -f Dockerfile.7.4.4-cli --build-arg ENVIRONMENT=development -t mgdteam/php:7.4.4-cli .
 
 docker push mgdteam/php:7.4.4-fpm-nginx
 docker push mgdteam/php:7.4.4-cli
