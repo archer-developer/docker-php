c6cb07aa20e2a60f85f8dacba12bf6b18d7e28e2
Alexander Samusevich <archer.developer@gmail.com>
2020-11-23 10:20:52 +0300

    PHP 8RC5 images were added

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env sh
 
-docker build -f Dockerfile.7.4.4-fpm --build-arg ENVIRONMENT=development -t mgdteam/php:7.4.4-fpm-nginx .
-docker build -f Dockerfile.7.4.4-cli --build-arg ENVIRONMENT=development -t mgdteam/php:7.4.4-cli .
+docker build -f Dockerfile.fpm \
+  --build-arg PHP_VERSION=7.4.4 \
+  -t mgdteam/php:7.4.4-fpm-nginx .
+
+docker build -f Dockerfile.cli \
+  --build-arg PHP_VERSION=7.4.4 \
+  -t mgdteam/php:7.4.4-cli .
+
+docker build -f Dockerfile.fpm \
+  --build-arg PHP_VERSION=8.0.0RC5 \
+  -t mgdteam/php:8.0.0RC5-fpm-nginx .
+
+docker build -f Dockerfile.cli \
+  --build-arg PHP_VERSION=8.0.0RC5 \
+  -t mgdteam/php:8.0.0RC5-cli .
 
 docker push mgdteam/php:7.4.4-fpm-nginx
 docker push mgdteam/php:7.4.4-cli
+docker push mgdteam/php:8.0.0RC5-fpm-nginx
+docker push mgdteam/php:8.0.0RC5-cli
