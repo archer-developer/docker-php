bc2ee86c0460a9e62188a0e9980a5acc3c08944c
Alexander Samusevich <archer.developer@gmail.com>
2022-07-30 16:57:56 +0300

    Build script is fixed

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 
 docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile.fpm \
   --build-arg PHP_VERSION=7.2 \
-  -t mgdteam/php:7.2-fpm-nginx .
+  -t mgdteam/php:7.2-fpm-nginx . \
+  --push
 
 docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile.cli \
   --build-arg PHP_VERSION=7.2 \
-  -t mgdteam/php:7.2-cli .
-
-docker push mgdteam/php:7.2-fpm-nginx
-docker push mgdteam/php:7.2-cli
+  -t mgdteam/php:7.2-cli . \
+  --push
