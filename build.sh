792dd3bdbee7d35ce72c103d1616542abbba2a0b
Alexander Samusevich <archer.developer@gmail.com>
2022-07-30 18:52:36 +0300

    Multiarch build is implemented: linux/amd64, linux/arm64, linux/armv7 are added

--- build.sh
+++ build.sh
@@ -1,32 +1,45 @@
 #!/usr/bin/env sh
 
-docker build -f Dockerfile.fpm \
+PLATFORMS=linux/amd64,linux/arm64,linux/arm/v7
+
+docker buildx build \
+  --platform ${PLATFORMS} \
+  -f Dockerfile.fpm \
   --build-arg PHP_VERSION=7.4 \
-  -t mgdteam/php:7.4-fpm-nginx .
+  -t mgdteam/php:7.4-fpm-nginx . \
+  --push
 
-docker build -f Dockerfile.cli \
+docker buildx build \
+  --platform ${PLATFORMS} \
+  -f Dockerfile.cli \
   --build-arg PHP_VERSION=7.4 \
-  -t mgdteam/php:7.4-cli .
+  -t mgdteam/php:7.4-cli . \
+  --push
 
-docker build -f Dockerfile.fpm \
+docker buildx build \
+  --platform ${PLATFORMS} \
+  -f Dockerfile.fpm \
   --build-arg PHP_VERSION=8.0 \
-  -t mgdteam/php:8.0-fpm-nginx .
+  -t mgdteam/php:8.0-fpm-nginx . \
+  --push
 
-docker build -f Dockerfile.cli \
+docker buildx build \
+  --platform ${PLATFORMS} \
+  -f Dockerfile.cli \
   --build-arg PHP_VERSION=8.0 \
-  -t mgdteam/php:8.0-cli .
+  -t mgdteam/php:8.0-cli . \
+  --push
 
-docker build -f Dockerfile.fpm \
+docker buildx build \
+  --platform ${PLATFORMS} \
+  -f Dockerfile.fpm \
   --build-arg PHP_VERSION=8.1 \
-  -t mgdteam/php:8.1-fpm-nginx .
+  -t mgdteam/php:8.1-fpm-nginx . \
+  --push
 
-docker build -f Dockerfile.cli \
+docker buildx build \
+  --platform ${PLATFORMS} \
+  -f Dockerfile.cli \
   --build-arg PHP_VERSION=8.1 \
-  -t mgdteam/php:8.1-cli .
-
-docker push mgdteam/php:7.4-fpm-nginx
-docker push mgdteam/php:7.4-cli
-docker push mgdteam/php:8.0-fpm-nginx
-docker push mgdteam/php:8.0-cli
-docker push mgdteam/php:8.1-fpm-nginx
-docker push mgdteam/php:8.1-cli
+  -t mgdteam/php:8.1-cli . \
+  --push
